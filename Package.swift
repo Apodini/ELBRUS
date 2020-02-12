42b9ac8a2f4d51eebc7209c6710449113216e9f0
Tom Mirwald <tom.mirwald@tum.de>
2020-02-12 16:21:57 +0100

    minor changes

--- Package.swift
+++ Package.swift
@@ -9,18 +9,12 @@ let package = Package(
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ELBRUS",
-            targets: ["ELBRUS"])
+        .library(name: "ELBRUS", targets: ["ELBRUS"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/vapor/codable-kit.git", .branch("master"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ELBRUS",
             dependencies: ["CodableKit"]),
