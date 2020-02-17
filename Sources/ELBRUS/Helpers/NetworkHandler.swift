c0c79c21b3ccdd6c926b698a46909b5713bd4551
Tom Mirwald <tom.mirwald@tum.de>
2020-02-17 17:11:41 +0100

    added an authorization var

--- Sources/ELBRUS/Helpers/NetworkHandler.swift
+++ Sources/ELBRUS/Helpers/NetworkHandler.swift
@@ -10,13 +10,14 @@ import Foundation
 import Combine
 
 // MARK: - NetworkHandler
-/// Defines a protocol to represent all the network functionality that is used to communicate with a REST endpoint, the encoding strategy and the different network requests (get, post, put, delete) 
+/// Defines a protocol to represent all the network functionality that is used to communicate with a REST endpoint, the encoding/decoding strategy and the different network requests (get, post, put, delete) 
 public protocol NetworkHandler {
     associatedtype Encoder: TopLevelEncoder
     associatedtype Decoder: TopLevelDecoder
     
     var encoder: Encoder { get }
     var decoder: Decoder { get }
+    var authorization: Authorization { get }
     
     func get<Element: Codable>(on route: URL) -> AnyPublisher<[Element], Error>
     func post<Element: Codable>(_ element: Element, on route: URL) -> AnyPublisher<Element, Error>
